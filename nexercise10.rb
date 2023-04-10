192934417a49be4190fa6a6396ecdeab707c1296
Anisa Mansuri <anisamansuri@Patterns-MacBook-Air.local>
2023-04-10 21:25:16 +0530

    nthird

---
+++ nexercise10.rb
@@ -0,0 +1,22 @@
+# defining methods
+
+# def intro(name,age)
+#     puts ("Heyyy, Myself "+name+" and I am "+age.to_s+" years old")
+# end
+
+# intro("Anisa",23)
+
+########################################################
+
+#We also can give the default value for the parameters
+
+
+def intro(name="Anisa",age=23)
+    puts ("Heyyy, Myself "+name+" and I am "+age.to_s+" years old")
+end
+
+intro()
+
+########################################################
+
+
