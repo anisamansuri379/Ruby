f37e29a98f7d26d690c10dc41363f8e40b96f18f
Anisa Mansuri <anisamansuri@Patterns-MacBook-Air.local>
2023-04-11 12:32:45 +0530

    fourth

---
+++ OOPS/Class and Objects/exercise3.rb
@@ -0,0 +1,35 @@
+# It covers
+# 7)Class Variables
+# 8)Class Methods
+
+
+class Product
+
+     @@total_purchases=0
+    def initialize (brand, price)
+       @brand=brand
+       @price=price
+
+       @@total_purchases+=1
+    end
+
+    def product_info
+       "The price for the brand #{@brand} is Rs. #{@price}"
+    end
+
+    def count 
+        @@total_purchases
+    end
+end
+
+p1=Product.new("Zara",3500)
+puts p1.product_info
+count=p1.count
+puts count
+
+p2=Product.new("WestSide",78000)
+puts p2.product_info
+count=p2.count
+puts count
+
+
